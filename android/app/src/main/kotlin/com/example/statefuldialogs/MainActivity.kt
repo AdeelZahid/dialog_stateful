681f4b326ee4d16a1865744ab1b605ed506fc4c2
Adeel <m.adeel2144@gmail.com>
2021-08-16 15:20:07 +0500

    Flutter Statefull widget

---
+++ android/app/src/main/kotlin/com/example/statefuldialogs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.statefuldialogs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? textFieldVal;
+
+  static showSnackBar(context, String? text) {
+    var snackBar = SnackBar(content: Text('$text'));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Future<void> showInformationDialog(BuildContext context) async {
+    return await showDialog(
+        context: context,
+        builder: (context) {
+          final TextEditingController _textEditingController =
+              TextEditingController();
+          bool isChecked = false;
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              content: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        controller: _textEditingController,
+                        onSaved: (value) {
+                          textFieldVal = value;
+                        },
+                        onChanged: (value) {
+                          textFieldVal = value;
+                        },
+                        validator: (value) {
+                          return value!.isNotEmpty ? null : "Invalid Field";
+                        },
+                        decoration:
+                            InputDecoration(hintText: "Enter Some Text"),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Choice Box"),
+                          Checkbox(
+                              value: isChecked,
+                              onChanged: (checked) {
+                                setState(() {
+                                  isChecked = checked!;
+                                });
+                              })
+                        ],
+                      )
+                    ],
+                  )),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Okay'),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print('$textFieldVal and $isChecked');
+                      // Do something like updating SharedPreferences or User Settings etc.
+                      showSnackBar(context,
+                          '$textFieldVal and ${isChecked ? "Check" : "Uncheck"}');
+                      Navigator.of(context).pop();
+                    }
+                  },
+                ),
+              ],
+            );
+          });
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful Dialog"),
+      ),
+      body: Container(
+        child: Center(
+          child: FlatButton(
+              color: Colors.deepOrange,
+              onPressed: () async {
+                await showInformationDialog(context);
+              },
+              child: Text(
+                "Stateful Dialog",
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              )),
+        ),
+      ),
+    );
+  }
+}
